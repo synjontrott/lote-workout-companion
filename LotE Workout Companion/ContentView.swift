6cae056b8083678dad5e3cbcad8eb40af2fc957d
Synjon Trott <Synjon.trott@gmail.com>
2026-06-23 15:38:38 -0500

    Initial Commit

---
+++ LotE Workout Companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LotE Workout Companion
+//
+//  Created by Synjon Trott on 6/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LotE Workout Companion/LotE_Workout_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotE_Workout_CompanionApp.swift
+//  LotE Workout Companion
+//
+//  Created by Synjon Trott on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct LotE_Workout_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
