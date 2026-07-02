84d2275b7bc5acb0b2f424561be74ffea3c5ea9e
Synjon Trott <Synjon.trott@gmail.com>
2026-07-02 10:13:05 -0500

    Add ci_post_clone.sh to install Flutter and pods on Xcode Cloud

---
+++ cross_platform_build/ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# Install Flutter using git clone.
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Precache iOS artifacts.
+flutter precache --ios
+
+# Run pub get in the Flutter project directory.
+cd "$CI_PRIMARY_REPOSITORY_PATH/cross_platform_build"
+flutter pub get
+
+# Install CocoaPods dependencies.
+cd ios
+pod install
+
+exit 0
